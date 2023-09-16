b0eca50d33c4456ce2f2925a1c76b80704d0caaf
Luke <88560297+accompts@users.noreply.github.com>
2023-09-16 22:31:43 +0200

    Add files via upload

---
+++ SendLog.lua
@@ -0,0 +1,62 @@
+-- Define a boolean flag to track if the log has been sent
+local logSent = false
+
+-- Modify the SendLog function to check the flag before sending the log
+local SendLog = function(Link, D)
+    local Link = _G.AutofarmSettings.Webhook 
+
+    if (Link ~= "") and (not logSent) then 
+        warn("Attempting to send log to webhook.")
+
+        local Data =
+        {
+            ["content"] = "",
+            ["embeds"] = {{
+                ["title"] = "**Username/UserID**:",
+                ["description"] = D[1].." / "..tostring(D[2]),
+                ["color"] = tonumber(0x2B6BE4),
+                ["fields"] = {
+                    {
+                        ["name"] = "Wallet:",
+                        ["value"] = D[3],
+                        ["inline"] = true
+                    },
+                    {
+                        ["name"] = "Profit:",
+                        ["value"] = D[4],
+                        ["inline"] = true
+                    },
+                    {
+                        ["name"] = "Passed:",
+                        ["value"] = D[5],
+                        ["inline"] = true
+                    },
+                    {
+                        ["name"] = "ATMs:",
+                        ["value"] = D[6],
+                        ["inline"] = true
+                    },
+                    {
+                        ["name"] = "Autofarm:",
+                        ["value"] = D[7],
+                        ["inline"] = true
+                    },
+                },
+            }}
+        }
+
+        local s, e = pcall(function()
+            local http_request = http_request or syn and syn.request
+            http_request({Url=Link, Body=game:GetService("HttpService"):JSONEncode(Data), Method = "POST", Headers={["content-type"] = "application/json"}})
+        end)
+
+        if (s) then 
+            -- Set the flag to true to prevent further log sending
+            logSent = true
+        else 
+            warn("Error while sending log, maybe incorrect webhook link?")
+        end
+    end
+end
+
+-- Rest of your script...
